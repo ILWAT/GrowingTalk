e5b82076ed0d1608049adfede20455574ef5c183
J.H. Moon <moonjh587@naver.com>
2024-01-12 22:25:48 +0900

    [Feat] - #12 로그인 구현을 위한 네트워크 모델 코드 작성

--- GrowingTalk/Model/LoginModel.swift
+++ GrowingTalk/Model/LoginModel.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+struct LoginBodyModel: Encodable {
+    let email: String
+    let password: String
+    let deviceToken: String?
+}
+
+struct LoginResultModel_V1: Decodable {
+    let user_id: Int
+    let nickname: String
+    let accessToken: String
+    let refreshToken: String
+}
+
+struct LoginResultModel_V2: Decodable {
+    let user_id: Int
+    let email: String
+    let nickname: String
+    let profileImage: String?
+    let phone: String?
+    let vendor: String?
+    let createdAt: String
+    let token: Tokens
+}
