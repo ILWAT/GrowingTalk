2c7766485d0a3cc8d9e26f1cff537583897f3d4f
J.H. Moon <moonjh587@naver.com>
2024-01-12 22:29:39 +0900

    [Feat] - #12 로그인 VC 및 ViewModel 구현 with 이메일 유효성 검사, 비밀번호 유효성 검사를 통한 서버 통신 여부 결정

---
+++ GrowingTalk/Scene/Login/LoginViewController.swift
@@ -0,0 +1,118 @@
+//
+//  LoginVIewController.swift
+//  GrowingTalk
+//
+//  Created by 문정호 on 1/11/24.
+//
+
+import UIKit
+import RxCocoa
+import RxSwift
+
+import SwiftUI
+
+final class LoginViewController: BaseViewController {
+    //MARK: - UIProperties
+    private let closeButton = {
+        let item =  UIBarButtonItem(image: UIImage(systemName: "xmark"), style: .plain, target: nil, action: nil)
+        item.tintColor = .label
+        return item
+    }()
+    private let idLabelField = LabelTextField(labelString: "이메일", textFieldPlaceHolder: "이메일을 입력하세요")
+    
+    private let passowrdLabelField = LabelTextField(labelString: "비밀번호", textFieldPlaceHolder: "비밀번호를 입력하세요")
+    
+    private let loginButton = InteractionButton(titleString: "로그인", isActive: false)
+    
+    //MARK: - Properties
+    let viewModel = LoginViewModel()
+    
+    let disposeBag = DisposeBag()
+    
+    //MARK: - override
+    
+    //MARK: - VC Method
+    override func configure() {
+        super.configure()
+    }
+    
+    override func configureNavigation() {
+        let appearance = UINavigationBarAppearance()
+        appearance.backgroundColor = .white
+        
+        self.navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        
+        self.title = "이메일 로그인"
+        self.navigationItem.leftBarButtonItem = closeButton
+    }
+    
+    override func bind() {
+        let input = LoginViewModel.Input(
+            closeButtonTap: closeButton.rx.tap,
+            idText: idLabelField.textField.rx.text.orEmpty,
+            passwordText: passowrdLabelField.textField.rx.text.orEmpty,
+            loginButtonTap: loginButton.rx.tap
+        
+        )
+        
+        let output = viewModel.transform(input)
+        
+        output.closebuttonTap
+            .drive(with: self) { owner, _ in
+                owner.dismiss(animated: true)
+            }
+            .disposed(by: disposeBag)
+            
+        output.wrongType
+            .drive(with: self) { owner, result in
+                switch result {
+                case .success(_):
+                    [owner.idLabelField, owner.passowrdLabelField].forEach { view in
+                        view.chageLabelColor(color: .label)
+                    }
+                case .failure(let failType):
+                    switch failType{
+                    case .id:
+                        owner.idLabelField.chageLabelColor(color: .systemRed)
+                        owner.idLabelField.textField.becomeFirstResponder()
+                    case .password:
+                        owner.passowrdLabelField.chageLabelColor(color: .systemRed)
+                        owner.passowrdLabelField.textField.becomeFirstResponder()
+                    case .all:
+                        [owner.idLabelField, owner.passowrdLabelField].forEach { view in
+                            view.chageLabelColor(color: .systemRed)
+                        }
+                        owner.becomeFirstResponder()
+                    }
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        output.LoginButtonActive
+            .drive(with: self) { owner, active in
+                owner.loginButton.changedButtonValid(newValue: active)
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    //MARK: - UI Method
+    override func configureViewHierarchy() {
+        super.configureViewHierarchy()
+        self.view.addSubViews([idLabelField, passowrdLabelField, loginButton])
+    }
+    
+    override func configureViewConstraints() {
+        idLabelField.snp.makeConstraints { make in
+            make.horizontalEdges.top.equalTo(self.view.safeAreaLayoutGuide).inset(24)
+        }
+        passowrdLabelField.snp.makeConstraints { make in
+            make.horizontalEdges.equalTo(idLabelField)
+            make.top.equalTo(idLabelField.snp.bottom).offset(24)
+        }
+        loginButton.snp.makeConstraints { make in
+            make.horizontalEdges.equalTo(self.view.safeAreaLayoutGuide).inset(24)
+            make.bottom.equalTo(self.view.keyboardLayoutGuide).inset(24)
+            make.height.equalTo(44)
+        }
+    }
+}
