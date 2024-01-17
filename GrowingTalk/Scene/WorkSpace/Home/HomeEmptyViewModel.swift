1fe1e3ea6d7f6f08fb86271940afc9a614d326e8
J.H. Moon <moonjh587@naver.com>
2024-01-17 22:20:15 +0900

    [Feat] - #16 빈 홈화면 UI 구현

---
+++ GrowingTalk/Scene/WorkSpace/Home/HomeEmptyViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  HomeViewModel.swift
+//  GrowingTalk
+//
+//  Created by 문정호 on 1/17/24.
+//
+
+import Foundation
+
+final class HomeEmptyViewModel: ViewModelType {
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    func transform(_ input: Input) -> Output {
+        
+        return Output(
+            
+        )
+    }
+}
